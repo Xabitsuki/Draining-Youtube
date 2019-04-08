51bf6880acc94becab086cf8500e0b61a34d786a
Xabier <xabier.rubiato@epfl.ch>
2019-04-08 17:03:05 +0200

    Write script to test iter0 on train video

--- process_0.py
+++ process_0.py
@@ -4,10 +4,20 @@ url = 'https://www.youtube.com/watch?v=1Rq9b_bn6Bc'
 vid_id = url_to_id(url)
 vid_file = 'slice0.mp4'
 
+# The frames in the frames dir come from the slice0.mp4 file
+
 if __name__ == '__main__':
 
     # Iter 0
+    iter0(vid_id=vid_id)
 
+    path_dir = pth_vid_dir(vid_id=vid_id)
+    path_iter0 = os.path.join(path_dir, 'iter0')
+    path_sfm = os.path.join(path_iter0, 'sfm_data.json')
+    path_feat = os.path.join(path_iter0, 'features')
+    path_incr = os.path.join(path_dir, 'incremental')
+    make_dir(path_incr)
 
-
-
+    openmvg_incremental(path_sfm=path_sfm,
+                        path_matches=path_feat,
+                        path_incr=path_incr)
