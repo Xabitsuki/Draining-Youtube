a1bce08a52d0e7b9e1dcdfb53af372c79d4f609c
xabitsuki <xabier.rubiato@epfl.ch>
2019-04-24 15:11:20 +0200

    Start master script

--- master.py
+++ master.py
@@ -1,1 +1,12 @@
 from functions import *
+
+url_lst = []
+
+
+## Capture
+# Todo: download and prepare folder for the list
+
+## Drain
+# Todo: run first iter_0 for each video + split in sets
+# Todo: incremental looping and cleaning
+
