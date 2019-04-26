5ce94e337afc60a8dd766edd1e6e047ebeba1cd8
Xabier <xabier.rubiato@epfl.ch>
2019-04-26 18:09:12 +0200

    Correct capture() to use sample option, make_sets()

--- master.py
+++ master.py
@@ -1,18 +1,20 @@
-from functions import *
 from master_functions import *
-url_lst = []
+url_lst = ['https://www.youtube.com/watch?v=9Ax-JrJ06fU']
 id_lst = []
+sample=True
 
-## Capture
-# Todo: download and prepare folder for the list
-for url in url_lst:
-    v_id = capture(url)
-    id_lst.append(v_id)
+if __name__ == '__main__':
+    ## Capture
+    # Todo: download and prepare folder for the list
+    for url in url_lst:
+        v_id = capture(url, sample=sample)
+        id_lst.append(v_id)
 
-## Drain
-# Todo: run first iter_0 for each video + split in sets
+    ## Drain
+    # Todo: run first iter_0 for each video + split in sets
+    for id in id_lst:
+        iter0(id)
+        make_sets(id)
 
-
-
-# Todo: incremental looping and cleaning
+    # Todo: incremental looping and cleaning
 
