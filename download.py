54dded291998a1a6dfe8e42ca75e96cca466baf8
Xabier <xabier.rubiato@epfl.ch>
2019-05-06 16:31:03 +0200

    Correct download script

--- download.py
+++ download.py
@@ -1,7 +1,13 @@
 from functions import yt_dl
 
-print('Start download of playlist : {}\n'.format(PLAYLIST))
-yt_dl(url= URL,
-      playlist=PLAYLIST,
-      n_items= N_ITEMS)
-print('Finish download of playlist : {}\n'.format(PLAYLIST))
+URL = 'https://www.youtube.com/watch?v=FVj8yg9ZVMA&list=PLHEt6c_hdjLtU0Qr41tOVzDJPTwbAx0zu''
+PLAYLIST = 'SHIBUYA STREET VIEW - WALKING TOUR'
+N_ITEMS = 10
+
+if __name__ == '__main__':
+
+    print('Start download of playlist : {}\n'.format(PLAYLIST))
+    yt_dl(url= URL,
+          playlist=PLAYLIST,
+          n_items= N_ITEMS)
+    print('Finish download of playlist : {}\n'.format(PLAYLIST))
