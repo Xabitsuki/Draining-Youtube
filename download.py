f19850357d8d3f88204491efa8e76469c43d4b8f
Xabier <xabier.rubiato@epfl.ch>
2019-05-06 16:31:18 +0200

    Correct download script

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 from functions import yt_dl
 
-URL = 'https://www.youtube.com/watch?v=FVj8yg9ZVMA&list=PLHEt6c_hdjLtU0Qr41tOVzDJPTwbAx0zu''
+URL = 'https://www.youtube.com/watch?v=FVj8yg9ZVMA&list=PLHEt6c_hdjLtU0Qr41tOVzDJPTwbAx0zu'
 PLAYLIST = 'SHIBUYA STREET VIEW - WALKING TOUR'
 N_ITEMS = 10
 
