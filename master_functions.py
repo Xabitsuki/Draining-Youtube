17846faf2b08dd6957da92ce58532933072f01ea
Xabier <xabier.rubiato@epfl.ch>
2019-05-25 17:55:09 +0200

    Save repo state

--- master_functions.py
+++ master_functions.py
@@ -1,5 +1,5 @@
 import os
-from multiprocessing import  Process
+from multiprocessing import Manager, Process
 
 from functions import *
 
@@ -66,7 +66,6 @@ def iter0(path_vid, rate, frame_force=False, feature_force=False, match_force=Fa
 
     # Compute  matches
     if match_force or not os.path.isfile(pth_iter0_mtchs(v_id, plylst)):
-
         path_sfm = os.path.join(pth_it0, 'sfm_data.json')
         cmd = 'openMVG_main_ComputeMatches -i {} -o {}'.format(path_sfm, path_feat)
         os.system(cmd)
@@ -102,6 +101,27 @@ def sfm_pipe(pth_set, width):
 
     openmvg_colors(pth_incr=path_incr)
 
+##
+
+
+def drain(rate, f_frms, f_ftrs, f_mtchs, sample, plylsts=[], vids=[]):
+    print('Start Draining')
+    execution = Process(target=launching, args=(rate, f_frms, f_ftrs, f_mtchs, sample, plylsts, vids))
+    execution.start()
+    execution.join()
+    print('Finish Draining')
+
+
+def launching(rate, f_frms, f_ftrs, f_mtchs, sample, plylsts=[], vids=[]):
+    for plylst in plylsts:
+        for v_id in os.listdir(pth_plylst(plylst)):
+            args = (pth_vid(v_id, plylst), rate, f_frms, f_ftrs, f_mtchs, sample)
+            Process(target=master_iter0, args=args).start()
+
+    for v_id in vids:
+        args = (pth_vid(v_id), rate, f_frms, f_ftrs, f_mtchs, sample)
+        Process(target=master_iter0, args=(args,)).start()
+
 
 def master_iter0(args):
     """Performs iter0() and launch sfm_pipe processes with
@@ -114,102 +134,43 @@ def master_iter0(args):
         args = (pth_set, width)
         Process(target=sfm_pipe, args=args).start()
 
-def launching(rate, f_frms, f_ftrs, f_mtchs, sample, plylsts=[], vids=[]):
-    for plylst in plylsts:
-        for v_id in os.listdir(pth_plylst(plylst)):
-            args = (pth_vid(v_id, plylst), rate, f_frms, f_ftrs, f_mtchs, sample)
-            Process(target=master_iter0, args=args).start()
 
-    for v_id in vids:
-        args = (pth_vid(v_id), rate, f_frms, f_ftrs, f_mtchs, sample)
-        Process(target=master_iter0, args=(args,)).start()
-
-def drain(rate, f_frms, f_ftrs, f_mtchs, sample, plylsts=[], vids=[]):
-    print('Start Draining')
-    execution = Process(target=launching, args=(rate, f_frms, f_ftrs, f_mtchs, sample, plylsts, vids))
-    execution.start()
-    execution.join()
-    print('Finish Draining')
-
-#####################################################################################################
-
-def iter0(path_vid, rate, frame_force=False, feature_force=False, match_force=False, sample=False):
-    """Function used to make the first iteration of processing loop.
-    Return: path_sets, width"""
-    v_id, plylst = get_plylst_id(path_vid)
-
-    # Extract frames
-    if frame_force or not os.path.isdir(pth_frms(v_id, plylst)):
-        xtrct_frame(v_id, plylst, sample, rate)
-
-    # Make iter0 dir
-    pth_it0 = pth_iter0(v_id, plylst)
-    make_dir(pth_it0)
-
-    # Listing
-    path_frms = pth_frms(v_id, plylst)
-    width = get_dic_info(path_vid)['width']
-    cmd = "openMVG_main_SfMInit_ImageListing -i {} -o {} -f {}".format(path_frms, pth_it0, width)
-    os.system(command=cmd)
-
-    # Compute features
-    path_feat = pth_iter0_feats(v_id, plylst)
-    if feature_force or not os.path.isdir(path_feat):
-        if os.path.isfile(path_feat):
-            os.system('rm -rf path_feat')
-        make_dir(path_feat)
-
-        path_sfm = os.path.join(pth_it0, 'sfm_data.json')
-        cmd = 'openMVG_main_ComputeFeatures -i {} -o {}'.format(path_sfm, path_feat)
-        os.system(cmd)
-
-    # Compute  matches
-    if match_force or not os.path.isfile(pth_iter0_mtchs(v_id, plylst)):
-
-        path_sfm = os.path.join(pth_it0, 'sfm_data.json')
-        cmd = 'openMVG_main_ComputeMatches -i {} -o {}'.format(path_sfm, path_feat)
-        os.system(cmd)
-
-def iter0_seq(args_iter0, shared_list):
-    """Calls iter= and append the return to the shared_list"""
-    paths_sets, width = iter0(*args_iter0)
-    for pth in paths_sets:
-        shared_list.append((pth, width))
+##
 
 
-def launching_seq_aux(target, args_l):
-    """ Function used to launch Processes in parallel"""
-    for args in args_l:
-        Process(target=target, args=args).start()
+def launching_seq(vids_list=[], plylst ='', rate=1, cpu_nbr=8, f_frms=True, f_ftrs=True, f_mtchs=True, sample=False):
+    """Function that spawns at maximum a number of processes equal to the parameter cpu_nbr.
+    Use : pass either a list of vid ids or the name of playlist"""
 
-
-def launching_seq(vids_list, rates=1, cpu_nbr=8, f_frms=True, f_ftrs=True, f_mtchs=True, sample=False):
-    """Function that spawns at maximum a number of processes equal to the parameter cpu_nbr."""
     manager = Manager()
     shared_list = manager.list()
 
-    # Repeat until all vids have been processed
+    # Generate vids paths from playlist
+    if plylst:
+        for el in os.listdir(pth_plylst(plylst)):
+            vids_list.append(el)
 
+    # Repeat until all videos have been processed
     target = iter0_seq
-    while not vids_list.empty():
-
+    while not len(vids_list) == 0:
         args_l=[]
         if len(vids_list) >= cpu_nbr:
             for i in range(cpu_nbr):
-                args_iter0 = (pth_vid(v_id=vids_list.pop), rates[-i-1], f_frms, f_ftrs, f_mtchs, sample)
+                args_iter0 = (pth_vid(v_id=vids_list.pop(), plylst=plylst), rate, f_frms, f_ftrs, f_mtchs, sample)
                 args_l.append((args_iter0, shared_list))
 
         else: # less videos than cpu_numbers
             for i in range(len(vids_list)):
-                args_l.append((pth_vid(v_id=vids_list.pop), rates[-i - 1], f_frms, f_ftrs, f_mtchs, sample))
+                args_iter0 = (pth_vid(v_id=vids_list.pop(), plylst=plylst), rate, f_frms, f_ftrs, f_mtchs, sample)
+                args_l.append((args_iter0, shared_list))
 
         control_launch = Process(target=launching_seq_aux, args=(target, args_l))
         control_launch.start()
         control_launch.join()
 
-    # Finish iter0 part
-    target = sfm_pipe
-    while not sfm_pipe_list.empty():
+    # Finish until all sets have been processed
+    target = sfm_pipe_seq
+    while not len(shared_list) == 0:
 
         args_l = []
         if len(shared_list) >= cpu_nbr:
@@ -224,3 +185,30 @@ def launching_seq(vids_list, rates=1, cpu_nbr=8, f_frms=True, f_ftrs=True, f_mtc
         control_launch = Process(target=launching_seq_aux, args=(target, args_l))
         control_launch.start()
         control_launch.join()
+
+
+def launching_seq_aux(target, args_l):
+    """ Function used to launch Processes in parallel"""
+
+    for args in args_l:
+        Process(target=target, args=args).start()
+
+
+def iter0_seq(args_iter0, shared_list):
+    """Calls iter= and append the return to the shared_list"""
+
+    paths_sets, width = iter0(*args_iter0)
+    sets = os.listdir(paths_sets)
+
+    for pth in [os.path.join(paths_sets, set) for set in sets]:
+        shared_list.append((pth, width))
+
+
+def sfm_pipe_seq(pth_sets, width):
+    pth_sets = [os.path.join(pth_sets, el) for el in os.listdir(pth_sets)]
+
+    for pth_set in pth_sets:
+        args = (pth_set, width)
+        current = Process(target=sfm_pipe, args=args)
+        current.start()
+        current.join()
